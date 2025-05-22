94ff12f06357d98f500b8c5596dc2a7a91be6081
Project  Nebula <maxoncbraun@icloud.com>
2025-05-22 10:25:47 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://musical-space-rotary-phone-r4949xxwgg4hpx6g-8080.app.github.dev/',
+    bare:'https://nebula-x.above.gay/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
