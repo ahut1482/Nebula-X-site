109b9c06d58cebea3ec6931df2b422c288f6abf0
Project  Nebula <maxoncbraun@icloud.com>
2025-05-22 09:46:02 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://nebula-x.above.gay/bare/',
+    bare:'https://goto.zkit.above.gay',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
